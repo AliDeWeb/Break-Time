4d8dfb77323cff821a7b1e4ce101dc19ca7d250d
Ali Moradi <Alimoradi0business@gmail.com>
2023-11-24 08:21:31 -0700

    save theme in localstorage feature added

--- scripts/app.js
+++ scripts/app.js
@@ -2,12 +2,23 @@ const $ = document;
 
 const bodySelector = $.querySelector(`body`);
 
+window.addEventListener(`load`, () => {
+  if (JSON.parse(localStorage.darkTheme) === true) {
+    $.documentElement.classList.add(`dark`);
+  }
+});
+
+// light && dark mode handler
 bodySelector.addEventListener(`click`, (e) => {
   if (
     e.target.classList.contains(`h-screen`) ||
     e.target.classList.contains(`container`)
   ) {
     $.documentElement.classList.toggle(`dark`);
-    console.log(e.target);
+    if ($.documentElement.classList.contains(`dark`)) {
+      localStorage.darkTheme = JSON.stringify(true);
+    } else {
+      localStorage.darkTheme = JSON.stringify(false);
+    }
   }
 });
