03b377739e87904227292080a1e8cfffa3454891
Ali Moradi <Alimoradi0business@gmail.com>
2023-11-24 08:45:45 -0700

    dynamic date feature added

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,32 @@
+import { months, days } from "./dates.js";
+
 const $ = document;
 
 const bodySelector = $.querySelector(`body`);
+
+const hourSelector = $.getElementById(`hour`);
+const minuteSelector = $.getElementById(`minute`);
+const secondSelector = $.getElementById(`second`);
+const yearSelector = $.getElementById(`year`);
+const monthSelector = $.getElementById(`month`);
+const daySelector = $.getElementById(`day`);
+
+let date;
 
 window.addEventListener(`load`, () => {
   if (JSON.parse(localStorage.darkTheme) === true) {
     $.documentElement.classList.add(`dark`);
   }
+
+  setInterval(() => {
+    date = new Date();
+    hourSelector.innerHTML = date.getHours();
+    minuteSelector.innerHTML = date.getMinutes();
+    secondSelector.innerHTML = date.getSeconds();
+    yearSelector.innerHTML = date.getFullYear();
+    monthSelector.innerHTML = months[date.getMonth()];
+    daySelector.innerHTML = days[date.getDay()];
+  }, 1000);
 });
 
 // light && dark mode handler
